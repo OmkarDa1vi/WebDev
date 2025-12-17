93c08a43381209ce57752fe8f8fafde37076abb6
Omkar <iomkarsdalvi@gmail.com>
2025-12-17 14:29:42 +0530

    Refactor Layout component and update main entry point

--- React/Card/src/App.jsx
+++ React/Card/src/App.jsx
@@ -1,19 +1,35 @@
-import "./App.css";
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
 
-function App() {
+export default function Layout() {
   const product = {
     productName: "Car",
     productDes: "This is a car",
   };
+
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        gap: "20px",
+        padding: "20px",
+        flexWrap: "wrap",
+      }}
+    >
       <Card data={product} />
       <Card
         data={{ ...product, productName: "Bike", productDes: "This is a bike" }}
       />
-    </>
+      <Card
+        data={{
+          ...product,
+          productName: "Truck",
+          productDes: "This is a truck",
+        }}
+      />
+    </div>
   );
 }
-
-export default App;
