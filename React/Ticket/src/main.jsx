5616ac6301d4ed26f5bafc75b3e37976a092bbd7
Omkar <iomkarsdalvi@gmail.com>
2026-01-01 19:44:35 +0530

    feat: Add new React Music Player application and update project configuration files.

---
+++ React/Ticket/src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import App from "./App.jsx";
+import {BrowserRouter} from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
