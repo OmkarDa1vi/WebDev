3ede7ec953c937cb39db3e93e15a94f057794195
Omkar <iomkarsdalvi@gmail.com>
2026-01-01 19:46:09 +0530

    First commit

--- React/MusicPlayer/src/App.jsx
+++ React/MusicPlayer/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const songs = [
   { id: 1, length: 100, title: "Closer", artist: "The Chainsmokers" },
@@ -48,17 +49,28 @@ function App() {
 
   return (
     <>
-      <div className="player">
-        <div className="song">
-          <h1>{songs[currentSong].title}</h1>
-          <h3>{songs[currentSong].artist}</h3>
+      <div className="mainLayout">
+        <div className="playlist">
+          {songs.map((song) => (
+            <div key={song.id}>
+              <h1>{song.title}</h1>
+              <h3>{song.artist}</h3>
+              <button onClick={() => setCurrentSong(song.id - 1)}>Play</button>
+            </div>
+          ))}
         </div>
-        <audio ref={audioRef} onTimeUpdate={handleTime} onEnded={nextSong} />
-        <progress value={progress} max={songs[currentSong].length}></progress>
-        <div className="controls">
-          <button onClick={lastSong}>Last Song</button>
-          <button className="play" onClick={playSong}>{play ? "Pause" : "Play"}</button>
-          <button onClick={nextSong}>Next</button>
+        <div className="player">
+          <div className="song">
+            <h1>{songs[currentSong].title}</h1>
+            <h3>{songs[currentSong].artist}</h3>
+          </div>
+          <audio ref={audioRef} onTimeUpdate={handleTime} onEnded={nextSong} />
+          <progress value={progress} max={songs[currentSong].length}></progress>
+          <div className="controls">
+            <button onClick={lastSong}>Last Song</button>
+            <button className="play" onClick={playSong}>{play ? "Pause" : "Play"}</button>
+            <button onClick={nextSong}>Next</button>
+          </div>
         </div>
       </div>
     </>
--- React/Ticket/src/main.jsx
+++ React/Ticket/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import App from "./App.jsx";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
